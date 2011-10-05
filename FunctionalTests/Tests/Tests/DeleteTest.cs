8ca889153b51c32fec81f9da49a5853ebf449748
kokovin <kokovin@skbkontur.ru>
2011-10-05 10:09:53 +0000

    написал еще один тест на удаление

--- FunctionalTests/Tests/Tests/DeleteTest.cs
+++ FunctionalTests/Tests/Tests/DeleteTest.cs
@@ -39,5 +39,36 @@ namespace Tests.Tests
                 Assert.AreEqual(1, keys.Length);
             }
         }
+
+        [Test, Description("После удаления всех колонок из row сам row также удаляется после compaction'а")]
+        public void RangeGhostsTestVersion2()
+        {
+            using (var conn = cassandraCluster.RetrieveColumnFamilyConnection(Constants.KeyspaceName, Constants.ColumnFamilyName))
+            {
+                conn.AddColumn("qxx", new Column
+                {
+                    Name = "qzz",
+                    Timestamp = 10,
+                    Value = new byte[] { 2, 3, 4 }
+                });
+                conn.AddColumn("qxx", new Column
+                {
+                    Name = "qtt",
+                    Timestamp = 10,
+                    Value = new byte[] { 2, 3, 4 }
+                });
+                conn.AddColumn("qxx", new Column
+                {
+                    Name = "qqq",
+                    Timestamp = 10,
+                    Value = new byte[] { 2, 3, 4 }
+                });
+                conn.DeleteBatch("qxx", new[] {"qzz", "qtt", "qqq"}, 10000);
+                var columns = conn.GetRow("qxx", null, 20);
+                Assert.AreEqual(0, columns.Length);
+                var keys = conn.GetKeys("", 100);
+                Assert.AreEqual(1, keys.Length);
+            }
+        }
     }
 }
