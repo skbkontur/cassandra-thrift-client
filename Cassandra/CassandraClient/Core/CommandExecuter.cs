46ce09ac12208976afe793be0433125399f3c844
ffominyh <ffominyh@skbkontur.ru>
2011-10-07 04:14:59 +0000

    Добавляем FierceIp в общий список еще разик

--- Cassandra/CassandraClient/Core/CommandExecuter.cs
+++ Cassandra/CassandraClient/Core/CommandExecuter.cs
@@ -12,50 +12,49 @@ namespace CassandraClient.Core
 {
     public class CommandExecuter : ICommandExecuter
     {
-        private ILog logger = LogManager.GetLogger("CommandExecuter");
-
         public CommandExecuter(IClusterConnectionPool clusterConnectionPool, IEndpointManager endpointManager, ICassandraClusterSettings settings)
         {
             this.clusterConnectionPool = clusterConnectionPool;
             this.endpointManager = endpointManager;
             this.settings = settings;
             recognizer = new CassandraClientExceptionTypeRecognizer();
-            foreach (var ipEndPoint in settings.Endpoints)
+            foreach(var ipEndPoint in settings.Endpoints)
                 this.endpointManager.Register(ipEndPoint);
+            this.endpointManager.Register(settings.EndpointForFierceCommands);
         }
 
         public void Execute(ICommand command)
         {
             logger.DebugFormat("Start executing {0} command.", command.GetType());
             ValidationResult validationResult = command.Validate();
-            if (validationResult.Status != ValidationStatus.Ok)
+            if(validationResult.Status != ValidationStatus.Ok)
                 throw new CassandraClientInvalidRequestException(validationResult.Message);
-            for (int i = 0; i < settings.Attempts; ++i)
+            for(int i = 0; i < settings.Attempts; ++i)
             {
                 IPEndPoint endpoint = command.IsFierce ? settings.EndpointForFierceCommands : endpointManager.GetEndPoint();
                 try
                 {
-                    using (var thriftConnection = clusterConnectionPool.BorrowConnection(new ConnectionPoolKey{IpEndPoint = endpoint, Keyspace = command.Keyspace}))
+                    using(var thriftConnection = clusterConnectionPool.BorrowConnection(new ConnectionPoolKey {IpEndPoint = endpoint, Keyspace = command.Keyspace}))
                         thriftConnection.ExecuteCommand(command);
                     endpointManager.Good(endpoint);
                     return;
                 }
-                catch (Exception e)
+                catch(Exception e)
                 {
                     string message = string.Format("An error occured while executing cassandra command '{0}'", command.GetType());
                     var exception = CassandraExceptionTransformer.Transform(e, message);
                     logger.Warn("Attempt " + i + " on " + endpoint + " failed.", exception);
                     endpointManager.Bad(endpoint);
-                    if (!recognizer.IsExceptionRetryable(exception))
+                    if(!recognizer.IsExceptionRetryable(exception))
                         throw exception;
-                    if (i + 1 == settings.Attempts)
+                    if(i + 1 == settings.Attempts)
                         throw new CassandraAttemptsException(settings.Attempts, exception);
                 }
             }
             logger.DebugFormat("Executing {0} command complete.", command.GetType());
-
         }
 
+        private readonly ILog logger = LogManager.GetLogger("CommandExecuter");
         private readonly IClusterConnectionPool clusterConnectionPool;
         private readonly IEndpointManager endpointManager;
         private readonly ICassandraClusterSettings settings;
