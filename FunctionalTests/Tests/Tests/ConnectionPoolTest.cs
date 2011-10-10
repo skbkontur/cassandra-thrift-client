81b1e5c7ad531ad8c61431a285aaf07d8270a868
ffominyh <ffominyh@skbkontur.ru>
2011-10-10 12:56:43 +0000

    багфикс

--- FunctionalTests/Tests/Tests/ConnectionPoolTest.cs
+++ FunctionalTests/Tests/Tests/ConnectionPoolTest.cs
@@ -15,7 +15,9 @@ namespace SKBKontur.Cassandra.FunctionalTests.Tests
         public void Test()
         {
             var threads = new List<Thread>();
-            for (int i = 0; i < 100; i++)
+            const int threadCount = 100;
+            finished = new int[threadCount];
+            for (int i = 0; i < threadCount; i++)
             {
                 int i1 = i;
                 var thread = new Thread(() => FillColumnFamily(i1));
@@ -41,28 +43,43 @@ namespace SKBKontur.Cassandra.FunctionalTests.Tests
 
                 var flag = threads.Aggregate(false, (current, thread) => current || (thread.IsAlive));
                 if (!flag) break;
+                for (int i = 0; i < threadCount; i++)
+                    Assert.AreNotEqual(-1, finished[i]);
             }
+            for (int i = 0; i < threadCount; i++)
+                Assert.AreEqual(1, finished[i]);
             Console.WriteLine(string.Format("Max free = {0}; Max busy: {1}", maxFree, maxBusy));
         }
 
+        public volatile int[] finished;
+
         private void FillColumnFamily(int id)
         {
-            for(int i = 0; i < 100; i++)
+            try
             {
-                using(var connection = cassandraCluster.RetrieveColumnFamilyConnection(Constants.KeyspaceName, Constants.ColumnFamilyName))
+                for(int i = 0; i < 100; i++)
                 {
-                    var list = new List<Column>();
-                    for(int j = 0; j < 1000; j++)
+                    using(var connection = cassandraCluster.RetrieveColumnFamilyConnection(Constants.KeyspaceName, Constants.ColumnFamilyName))
                     {
-                        list.Add(new Column
-                            {
-                                Name = string.Format("name_{0}_{1}_{2}", id, i, j),
-                                Value = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}
-                            });
+                        var list = new List<Column>();
+                        for(int j = 0; j < 1000; j++)
+                        {
+                            list.Add(new Column
+                                {
+                                    Name = string.Format("name_{0}_{1}_{2}", id, i, j),
+                                    Value = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}
+                                });
+                        }
+                        connection.AddBatch(string.Format("row_{0}_{1}", id, i), list);
                     }
-                    connection.AddBatch(string.Format("row_{0}_{1}", id, i), list);
                 }
             }
+            catch
+            {
+                finished[id] = -1;
+                throw;
+            }
+            finished[id] = 1;
         }
     }
 }
