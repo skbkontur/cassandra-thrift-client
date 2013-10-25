6650928fb0431b30b9b0bb9aa7b2e393a432cbc0
tihonove <tihonove@skbkontur.ru>
2013-10-25 12:58:59 +0600

    Избавился ещё от лишних классов

--- Cassandra/CassandraClient/Core/Pools/ConnectionPoolKey.cs
+++ Cassandra/CassandraClient/Core/Pools/ConnectionPoolKey.cs
@@ -40,40 +40,4 @@ namespace SKBKontur.Cassandra.CassandraClient.Core.Pools
             return string.Format("EndPoint={0}; KeySpace={1}, IsFierce={2}", IpEndPoint, Keyspace, IsFierce);
         }
     }
-
-    public class ConnectionKey : IEquatable<ConnectionKey>
-    {
-        public ConnectionKey(string keyspace, bool isFierce)
-        {
-            Keyspace = keyspace;
-            ISFierce = isFierce;
-        }
-
-        public bool Equals(ConnectionKey other)
-        {
-            if(ReferenceEquals(null, other)) return false;
-            if(ReferenceEquals(this, other)) return true;
-            return string.Equals(Keyspace, other.Keyspace) && IsFierce.Equals(other.IsFierce);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if(ReferenceEquals(null, obj)) return false;
-            if(ReferenceEquals(this, obj)) return true;
-            if(obj.GetType() != this.GetType()) return false;
-            return Equals((ConnectionKey)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((Keyspace != null ? Keyspace.GetHashCode() : 0) * 397) ^ IsFierce.GetHashCode();
-            }
-        }
-
-        public string Keyspace { get; set; }
-        public bool ISFierce { get; set; }
-        public bool IsFierce { get; set; }
-    }
 }
