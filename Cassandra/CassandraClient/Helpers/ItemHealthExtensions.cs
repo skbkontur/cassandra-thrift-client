61710fadcdca4d35b60a1721cea9709c536051db
tihonove <tihonove@skbkontur.ru>
2013-10-25 16:56:40 +0600

    Сняли ограничение с алгоритма ShuffleByHealth

--- Cassandra/CassandraClient/Helpers/ItemHealthExtensions.cs
+++ Cassandra/CassandraClient/Helpers/ItemHealthExtensions.cs
@@ -6,33 +6,35 @@ namespace SKBKontur.Cassandra.CassandraClient.Helpers
 {
     public static class ItemHealthExtensions
     {
-        public static T2 RandomItemByHealth<T, T2>(this IEnumerable<T> items, Func<T, double> healthSelector, Func<T, T2> resultSelector) where T2 : class
+        public static T2 RandomItemByHealth<T, T2>(this IEnumerable<T> items, Func<T, double> healthSelector, Func<T, T2> resultSelector)
         {
             return items.ShuffleByHealth(healthSelector, resultSelector).First();
         }
 
-        public static IEnumerable<T2> ShuffleByHealth<T, T2>(this IEnumerable<T> items, Func<T, double> healthSelector, Func<T, T2> resultSelector) where T2 : class
+        public static IEnumerable<T2> ShuffleByHealth<T, T2>(this IEnumerable<T> items, Func<T, double> healthSelector, Func<T, T2> resultSelector)
         {
             var itemsWithHealth = new HashSet<KeyValuePair<T, double>>(items.Select(x => new KeyValuePair<T, double>(x, healthSelector(x))));
             var totalItemListLength = itemsWithHealth.Count;
 
             for (var i = 0; i < totalItemListLength; i++)
             {
-                T2 result = null;
+                var result = default(T2);
                 var healthSum = itemsWithHealth.Sum(h => h.Value);
 
+                var valueFound = false;
                 var randomValue = Random.NextDouble();
                 foreach (var itemWithHealth in itemsWithHealth)
                 {
                     randomValue -= itemWithHealth.Value / healthSum;
                     if(randomValue < epsilon)
                     {
+                        valueFound = true;
                         result = resultSelector(itemWithHealth.Key);
                         itemsWithHealth.Remove(itemWithHealth);
                         break;
                     }
                 }
-                if (result == null)
+                if (!valueFound)
                 {
                     var last = itemsWithHealth.Last();
                     result = resultSelector(last.Key);
@@ -42,7 +44,7 @@ namespace SKBKontur.Cassandra.CassandraClient.Helpers
             }
         }
 
-        public static IEnumerable<T> ShuffleByHealth<T>(this IEnumerable<T> items, Func<T, double> healthSelector) where T: class
+        public static IEnumerable<T> ShuffleByHealth<T>(this IEnumerable<T> items, Func<T, double> healthSelector)
         {
             return items.ShuffleByHealth(healthSelector, x => x);
         }
