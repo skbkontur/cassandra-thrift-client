60b54e1964039810d98e963bfa3c4a4175403098
tihonove <tihonove@skbkontur.ru>
2013-10-30 18:19:43 +0600

    Обновил тест на работ со схемой

---
+++ FunctionalTests/Tests/Tests/SchemaTests/TestKeyspaceSchemaModificationCassandra.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+using NUnit.Framework;
+
+using SKBKontur.Cassandra.CassandraClient.Abstractions;
+using SKBKontur.Cassandra.CassandraClient.Clusters;
+using SKBKontur.Cassandra.FunctionalTests.Management;
+
+namespace SKBKontur.Cassandra.FunctionalTests.Tests.SchemaTests
+{
+    [TestFixture]
+    public class TestKeyspaceSchemaModificationCassandra
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            node = StartSingleCassandraSetUp.Node;
+            cluster = new CassandraCluster(node.CreateSettings(IPAddress.Loopback));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            cluster.Dispose();
+        }
+
+        [Test]
+        public void TestAddKeyspace()
+        {
+            using(var clusterConnection = cluster.RetrieveClusterConnection())
+            {
+                var keyspaceName = Guid.NewGuid().ToString();
+                clusterConnection.AddKeyspace(new Keyspace
+                {
+                    Name = keyspaceName,
+                    ReplicaPlacementStrategy = "org.apache.cassandra.locator.SimpleStrategy",
+                    ReplicationFactor = 1
+                });
+                var keyspaces = clusterConnection.RetrieveKeyspaces();
+                Assert.That(keyspaces.Any(x => x.Name == keyspaceName));                
+            }
+        }
+
+        private CassandraNode node;
+        private CassandraCluster cluster;
+    }
+}
