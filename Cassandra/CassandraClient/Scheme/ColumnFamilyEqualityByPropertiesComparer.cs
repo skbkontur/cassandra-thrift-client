986b1e9ea67ece50cd3f054a88b1889a4207bcf9
tihonove <tihonove@skbkontur.ru>
2013-10-31 14:22:36 +0600

    Добавил сравнивалку для ColumnFamily

---
+++ Cassandra/CassandraClient/Scheme/ColumnFamilyEqualityByPropertiesComparer.cs
@@ -0,0 +1,13 @@
+﻿using SKBKontur.Cassandra.CassandraClient.Abstractions;
+
+namespace SKBKontur.Cassandra.CassandraClient.Scheme
+{
+    internal class ColumnFamilyEqualityByPropertiesComparer
+    {
+        public bool Equals(ColumnFamily x, ColumnFamily y)
+        {
+            return
+                x.ReadRepairChance.Equals(y.ReadRepairChance);
+        }
+    }
+}
---
+++ Cassandra/Tests/CoreTests/ColumnFamilyEqualityByPropertiesComparerTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+using SKBKontur.Cassandra.CassandraClient.Abstractions;
+using SKBKontur.Cassandra.CassandraClient.Scheme;
+
+namespace Cassandra.Tests.CoreTests
+{
+    [TestFixture]
+    public class ColumnFamilyEqualityByPropertiesComparerTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            comparer = new ColumnFamilyEqualityByPropertiesComparer();
+        }
+
+        [Test]
+        public void TestCompareByReadRepairChanceProperty()
+        {
+            Assert.That(
+                comparer.Equals(
+                    new ColumnFamily {Name = "123", ReadRepairChance = 1.0},
+                    new ColumnFamily {Name = "123", ReadRepairChance = 1.0}
+                    )
+                );
+            Assert.That(
+                !comparer.Equals(
+                    new ColumnFamily {Name = "123", ReadRepairChance = 1.0},
+                    new ColumnFamily {Name = "123", ReadRepairChance = 1.1}
+                    )
+                );
+        }
+
+        private ColumnFamilyEqualityByPropertiesComparer comparer;
+    }
+}
