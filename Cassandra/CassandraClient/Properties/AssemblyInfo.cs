e7358e5d57239ee6139999032c7fed2bf1d615b5
tihonove <tihonove@skbkontur.ru>
2013-11-01 12:01:07 +0600

    Обновил версию сборки

--- Cassandra/CassandraClient/Properties/AssemblyInfo.cs
+++ Cassandra/CassandraClient/Properties/AssemblyInfo.cs
@@ -40,4 +40,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.2.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0 Release Candidate")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
