41e0212f20e4ed90e5aa4f746ad934dced234def
tihonove <tihonove@skbkontur.ru>
2014-02-17 13:23:26 +0600

    Поменял версию публикуемых сборок

--- Cassandra/ClusterDeployment/Properties/AssemblyInfo.cs
+++ Cassandra/ClusterDeployment/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClusterDeployment")]
+[assembly: AssemblyTitle("CassandraClusterDeployment")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClusterDeployment")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("SKBKontur")]
+[assembly: AssemblyProduct("CassandraClusterDeployment")]
+[assembly: AssemblyCopyright("Copyright © SKBKontur 2014")]
+[assembly: AssemblyTrademark("SKBKontur.Cassandra.ClusterDeployment")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
