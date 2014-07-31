2024a2fdea9e980db35772a0bd1577e5a81d391a
burmistrov <burmistrov@skbkontur.ru>
2014-07-31 15:48:27 +0600

    Рефакторинг GetRowsTest: вместо двух одинаковых тестов TestGetRows и TestGetRows2 сделал один тест TestGetRowsExclusive с параметром и два TestCase

--- FunctionalTests/Tests/Tests/GetRowsTest.cs
+++ FunctionalTests/Tests/Tests/GetRowsTest.cs
@@ -11,8 +11,9 @@ namespace SKBKontur.Cassandra.FunctionalTests.Tests
 {
     public class GetRowsTest : CassandraFunctionalTestBase
     {
-        [Test]
-        public void TestGetRows()
+        [TestCase(20)]
+        [TestCase(100)]
+        public void TestGetRowsExclusive(int columnsCount)
         {
             const int rowKeysCount = 100;
             const int columnNamesCount = 100;
@@ -33,7 +34,7 @@ namespace SKBKontur.Cassandra.FunctionalTests.Tests
 
             for(int i = 0; i < columnNamesCount; i++)
             {
-                List<KeyValuePair<string, Column[]>> res = columnFamilyConnection.GetRowsExclusive(rowKeys, IntToString(i), 100);
+                List<KeyValuePair<string, Column[]>> res = columnFamilyConnection.GetRowsExclusive(rowKeys, IntToString(i), columnsCount);
                 res.Sort((x, y) => String.Compare(x.Key, y.Key, StringComparison.Ordinal));
                 Assert.AreEqual(rowKeysCount, res.Count);
                 for(int j = 0; j < res.Count; j++)
@@ -41,45 +42,8 @@ namespace SKBKontur.Cassandra.FunctionalTests.Tests
                     KeyValuePair<string, Column[]> row = res[j];
                     Assert.AreEqual(row.Key, rowKeys[j]);
                     Column[] columns = row.Value;
-                    Assert.AreEqual(Math.Min(100, columnNamesCount - i - 1), columns.Length);
+                    Assert.AreEqual(Math.Min(columnsCount, columnNamesCount - i - 1), columns.Length);
                     for(int k = 0; k < columns.Length; k++)
-                        Assert.AreEqual(columns[k].Name, IntToString(k + i + 1));
-                }
-            }
-        }
-
-        [Test]
-        public void TestGetRows2()
-        {
-            const int rowKeysCount = 100;
-            const int columnNamesCount = 100;
-
-            string[] rowKeys = new int[rowKeysCount].Select(x => Guid.NewGuid().ToString()).ToArray();
-            Array.Sort(rowKeys);
-            foreach (string rowKey in rowKeys)
-            {
-                IEnumerable<Column> columns = new int[columnNamesCount].Select((x, i) => new Column
-                {
-                    Name = IntToString(i),
-                    Timestamp = DateTime.UtcNow.Ticks,
-                    Value = new byte[] { 1, 2, 3 }
-                });
-                IEnumerable<KeyValuePair<string, IEnumerable<Column>>> keyValuePairs = columns.Select(column => new KeyValuePair<string, IEnumerable<Column>>(rowKey, new[] { column }));
-                columnFamilyConnection.BatchInsert(keyValuePairs);
-            }
-
-            for (int i = 0; i < columnNamesCount; i++)
-            {
-                List<KeyValuePair<string, Column[]>> res = columnFamilyConnection.GetRowsExclusive(rowKeys, IntToString(i), 20);
-                res.Sort((x, y) => String.Compare(x.Key, y.Key, StringComparison.Ordinal));
-                Assert.AreEqual(rowKeysCount, res.Count);
-                for (int j = 0; j < res.Count; j++)
-                {
-                    KeyValuePair<string, Column[]> row = res[j];
-                    Assert.AreEqual(row.Key, rowKeys[j]);
-                    Column[] columns = row.Value;
-                    Assert.AreEqual(Math.Min(20, columnNamesCount - i - 1), columns.Length);
-                    for (int k = 0; k < columns.Length; k++)
                         Assert.AreEqual(columns[k].Name, IntToString(k + i + 1));
                 }
             }
