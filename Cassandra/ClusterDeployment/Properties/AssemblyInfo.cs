bc8565e6033ec1ed0bb74e49eaf0f9b606c885de
andrew <Andrew.Kostousov@gmail.com>
2015-02-26 21:07:02 +0500

    version 1.6.0

--- Cassandra/ClusterDeployment/Properties/AssemblyInfo.cs
+++ Cassandra/ClusterDeployment/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SKBKontur")]
 [assembly: AssemblyProduct("CassandraClusterDeployment")]
-[assembly: AssemblyCopyright("Copyright © SKBKontur 2014")]
+[assembly: AssemblyCopyright("Copyright © SKBKontur 2014-2015")]
 [assembly: AssemblyTrademark("SKBKontur.Cassandra.ClusterDeployment")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.6.0")]
-[assembly: AssemblyFileVersion("1.5.6.0")]
-[assembly: AssemblyInformationalVersion("1.5.6")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
