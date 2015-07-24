e7694d3fff04120ba6d4a9024b9ffaa77a50e322
andrew <Andrew.Kostousov@gmail.com>
2015-07-24 12:28:07 +0500

    version bump; + script to run cassandra

---
+++ run.cmd
@@ -0,0 +1,1 @@
+start CassandraClient.FunctionalTests\CassandraDeploymentManager\bin\Release\Cassandra.CassandraDeploymentManager.exe "%~dp0\Assemblies\Cassandra\Server" FullRedeploy "%~dp0\CassandraDeploy"
