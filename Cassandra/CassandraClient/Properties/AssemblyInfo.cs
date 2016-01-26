31455d2c95d12b150ffdb468bcbd08b41e8b3435
Nick Burlakov <burlakov.nick@gmail.com>
2016-01-26 17:25:50 +0500

    Обновил версию клиента

--- Cassandra/CassandraClient/Properties/AssemblyInfo.cs
+++ Cassandra/CassandraClient/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.7")]
-[assembly: AssemblyFileVersion("1.6.7.0")]
-[assembly: AssemblyInformationalVersion("1.6.7")]
+[assembly: AssemblyVersion("1.7.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
