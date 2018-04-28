67412f903b7a1e80a7b97aa48fc7e90163a520bb
Den Mukhametianov <den.mukhametianov@gmail.com>
2018-04-28 11:33:44 +0500

    build and ilmerge fix

--- ilmerge-cassandra-client.cmd
+++ ilmerge-cassandra-client.cmd
@@ -1,6 +1,6 @@
 set OUT_DIR=%~dp0Assemblies\merged
 set OUT_ASSSEMBLY=Cassandra.CassandraClient.dll
 set PRIMARY_ASSSEMBLY=Cassandra.CassandraClient.dll
-set DEPS=Apache.Cassandra.209.dll log4net.dll Metrics.dll Thrift.dll 
+set DEPS=Apache.Cassandra.209.dll Metrics.dll Thrift.dll
 set SRC_DIR=%~dp0Assemblies\
 call %~dp0ilmerge\merge.cmd || exit /b 1
--- ilmerge/merge.cmd
+++ ilmerge/merge.cmd
@@ -3,5 +3,5 @@ if exist %OUT_DIR% (
 )
 mkdir %OUT_DIR% || exit /b 1
 
-set TARGET_PLATFORM="v4, %ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5"
+set TARGET_PLATFORM="v4, %ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.1"
 %~dp0ILMerge.exe /wildcards /log:ilmerge.log %ADDITIONAL_ARGS% /targetplatform:%TARGET_PLATFORM% /lib:%SRC_DIR% /out:%OUT_DIR%\%OUT_ASSSEMBLY% %PRIMARY_ASSSEMBLY% %DEPS% || exit /b 1
