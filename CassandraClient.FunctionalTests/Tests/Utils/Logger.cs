69cb06e24926a2221b6680596a1df69d9bf8574e
avk <Andrew.Kostousov@gmail.com>
2018-05-09 19:56:13 +0500

    update Vostok.Logging - FileLog should work now

--- CassandraClient.FunctionalTests/Tests/Utils/Logger.cs
+++ CassandraClient.FunctionalTests/Tests/Utils/Logger.cs
@@ -13,17 +13,20 @@ namespace SKBKontur.Cassandra.FunctionalTests.Utils
     {
         private static ILog log;
 
-        public static ILog Instance => log ?? (log = new SilentLog());
+        public static ILog Instance => log ?? (log = InitFileLogger());
 
         private static ILog InitFileLogger()
         {
+            var logsDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
+            if(!Directory.Exists(logsDir))
+                Directory.CreateDirectory(logsDir);
             FileLog.Configure(() => new FileLogSettings
                 {
                     AppendToFile = false,
                     EnableRolling = false,
                     Encoding = Encoding.UTF8,
                     ConversionPattern = ConversionPattern.Default,
-                    FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", $"FunctionalTests-{DateTime.Now:yyyy-MM-dd.HH:mm:ss}.log"),
+                    FilePath = Path.Combine(logsDir, $"FunctionalTests-{DateTime.Now:yyyy-MM-dd.HH:mm:ss}.log"),
                 });
             return new FileLog();
         }
