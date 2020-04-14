6e58c3a439f7cf104bfa9d7b916730e83efc45dc
Andrew Kostousov <Andrew.Kostousov@gmail.com>
2020-04-14 20:56:56 +0500

    Update CassandraFunctionalTestBase.cs

--- Cassandra.ThriftClient.Tests/FunctionalTests/Tests/CassandraFunctionalTestBase.cs
+++ Cassandra.ThriftClient.Tests/FunctionalTests/Tests/CassandraFunctionalTestBase.cs
@@ -51,7 +51,7 @@ namespace Cassandra.ThriftClient.Tests.FunctionalTests.Tests
                                         }
                                 }
                         }
-                }, false);
+                }, changeExistingKeyspaceMetadata : false);
         }
 
         [TearDown]
