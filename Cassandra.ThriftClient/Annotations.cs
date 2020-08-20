7126750f901a8e309ec9228d0d3246323c5d79e2
iperevoschikov <iperevoschikov@skbkontur.ru>
2020-08-20 17:46:42 +0500

    Add code cleanup checks

--- Cassandra.ThriftClient/Annotations.cs
+++ Cassandra.ThriftClient/Annotations.cs
@@ -204,19 +204,22 @@ namespace JetBrains.Annotations
     }
 
     /// <summary>
-    /// This attribute is intended to mark publicly available API
-    /// which should not be removed and so is treated as used.
+    ///     This attribute is intended to mark publicly available API
+    ///     which should not be removed and so is treated as used.
     /// </summary>
     [MeansImplicitUse(ImplicitUseTargetFlags.WithMembers)]
     internal sealed class PublicAPIAttribute : Attribute
     {
-        public PublicAPIAttribute() { }
+        public PublicAPIAttribute()
+        {
+        }
 
         public PublicAPIAttribute([NotNull] string comment)
         {
             Comment = comment;
         }
 
-        [CanBeNull] public string Comment { get; }
+        [CanBeNull]
+        public string Comment { get; }
     }
 }
